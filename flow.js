6d3b76a331ad27d78e25fa3b52e9a7df073b1215
vzakharov <vzakharov@gmail.com>
2018-03-07 21:35:43 +0300

    Smartcat test environment

--- flow.js
+++ flow.js
@@ -1,3 +1,4 @@
+https = require('https')
 const _ = require('lodash')
 
 module.exports = async (a) => {
@@ -10,42 +11,66 @@ module.exports = async (a) => {
     }
 
     const statusTitles = {
-        created: "Created",             /// This and the next one refer to project/document statuses
-        completed: "Completed",         
+        created: "New",             /// This and the next one refer to project/document statuses
+        completed: '✔',         
         inProgress: "In progress"
     }
 
     with (a) {
+
+        //api.url = 'https://smartcat.ai/api/integration/v1/'
         
-        api.url = 'https://smartcat.ai/api/integration/v1/'
+        // For testing only:
+        api.options = {
+            baseURL: 'https://smartcat.stage-next.sc.local/api/integration/v1/',
+            auth: require('./credentials.json').smartcat_next,
+            httpsAgent: new https.Agent({  
+                rejectUnauthorized: false
+            })
+        }
+
+        jump(firstTime)
         
-        put("Hey, I’m **O**scar, the **S**mart**c**at-**a**ugmenting **r**obot.")
+        async function firstTime() {
+            put("Hey, I’m **O**scar, the **S**mart**c**at-**a**ugmenting **r**obot.")
     
-        vars.firstName = await read("And you must be ...?")
-    
-        put("Nice to meet you, $firstName!")
-    
-        put("Now let’s connect to your Smartcat account. Do you know your API credentials?")
+            vars.firstName = await read("And you must be ...?")
         
-        await choose([
-            "Yes",  getCreds,
-            "No", () => {}
-        ])
+            put("Nice to meet you, $firstName!")
         
+            put("Now let’s connect to your Smartcat account. Do you know your API credentials?")
+            
+            choose(
+                "Yes",  getCreds,
+                "No", () => {}
+            )
+    
+        }
+
         async function getCreds() {
             
-            // Object.assign(api, require('./credentials.json').smartcat)
-            Object.assign(api, {
-                username: await read("What’s your API username?"),
-                password: await read("And the password? (NB! Make sure to delete the message after sending!)")
-            })
+            // Object.assign(api, {
+            //     username: await read("What’s your API username?"),
+            //     password: await read("And the password? (NB! Make sure to delete the message after sending!)")
+            // })
     
             put("One second, let’s see if it works...")
-            let account = await api.get('account')
+            account = await api.get('account')
     
-            put("Yay, account *$name* successfully loaded!", account)
-    
-            await getProjects({initial: true})
+            jump(accountMenu)
+        }
+
+        async function accountMenu() {
+            projects = await api.get('project/list')
+            projects.reverse()
+
+            put("Yay, we’re inside the **$name** account. What do you want to do now?", account)
+
+            choose(
+                (projects.length > 0) && "Manage my projects", dummy,
+                "Create a new project", dummy,
+                "Manage my team", dummy
+            )
         }
     
         async function getProjects(options = {initial: false}) {
@@ -56,7 +81,7 @@ module.exports = async (a) => {
                 put("Fetching your projects...") /// Shown afterwards
             }
     
-            let projects = (await api.get('project/list')).reverse()   
+
 
             projects.forEach((p) => {
                 let msg
@@ -67,24 +92,31 @@ module.exports = async (a) => {
                     msg = statusTitles[p.status]
                 }
 
-                p.statusMessage = msg
+                p.statusStr = msg
+                p.wordCount = _.sum(p.documents.map(doc => doc.wordsCount))
+                let langs = p.targetLanguages
+                if (langs) {
+                    if (langs.length > 4) {
+                        p.langsStr = format("$num langs", {num:langs.length}) /// Indicates how many languages more there are
+                    } else {
+                        p.langsStr = langs.slice(0, 4).join("/")
+                    }
+                } else {
+                    p.langsStr = "_"
+                }
             })
 
             put("Here you go:")
             await choose([
-                "[Create a new project]", newProject,
+                "[Create a new project]", () => newProject,
                 "[Do something else]", mainMenu,
-                projects, '$name ($statusMessage)', showProject
+                projects, '$name ($statusStr, $sourceLanguage→$langsStr, $wordCount words)', showProject
             ])
 
         }
     
-        async function mainMenu() {
-
-        }
-
         async function newProject() {
-
+            
         }
 
         async function showProject(p) {
@@ -97,7 +129,7 @@ module.exports = async (a) => {
                 ["Name", p.name],
                 ["Source language", p.sourceLanguage],
                 ["Target language(s)", p.targetLanguages.join(', ')],
-                ["Word count", _.sum(p.documents.map(doc => doc.wordsCount))],
+                ["Word count", p.wordCount],
                 ["Deadline", p.deadline],
                 ["Created at", p.creationDate],
                 ["Status", statusTitles[p.status]]
